8f4bf98addaa19571ed3e525903d3c3466bb06a4
kill99999-dev <fire.99.mail@gmail.com>
2025-03-24 19:29:32 +0100

    Create 0001 sections.asm

---
+++ 0001 sections.asm
@@ -0,0 +1,6 @@
+section .data
+; Inserisci qui i dati ad esempio i testi dei messaggi
+section .text
+; Inserisci qui il codice
+section .bss
+; Inserisci qui le variabili non inizializzate e quindi senza valore
