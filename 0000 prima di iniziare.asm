e444a3e30ff342e621191211c2fff9ebe545fac6
kill99999-dev <fire.99.mail@gmail.com>
2025-03-24 20:05:50 +0100

    Update 0000 prima di iniziare.asm

--- 0000 prima di iniziare.asm
+++ 0000 prima di iniziare.asm
@@ -8,3 +8,9 @@
 ; A breve svilupperò anche la versione .html con una guida più semplice da utilizzare e meglio strutturata.
 ;
 ; Per iniziare, seleziona il primo file (0001 sezioni.asm) qui a fianco.
+; All'inizio ti sembrerà difficile, ma non perdere le speranze!
+; Una volta che ci prendi la mano, diventerà tutto molto più naturale.
+; Grazie agli esempi e agli esercizi inclusi in questa guida, imparare l'Assembly sarà un vero gioco da ragazzi!
+;
+; Ricorda che, per qualsiasi dubbio, potrai lasciare una issue. Il nostro team potrebbe metterci diverso tempo a rispondere.
+; Sei pregato di NON SOLLECITARE, altrimenti ignoreremo la richiesta.
