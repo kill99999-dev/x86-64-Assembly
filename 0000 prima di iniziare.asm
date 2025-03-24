c5beca398f716bf0a362550448dbe624d5f50fd4
kill99999-dev <fire.99.mail@gmail.com>
2025-03-24 19:48:58 +0100

    Create 0000 prima di iniziare.asm

---
+++ 0000 prima di iniziare.asm
@@ -0,0 +1,10 @@
+; Qui imparerai i concetti di base dell'Assembly.
+; Dopo poco tempo potrai subito creare il tuo primo programma in Assembly.
+; Ricorda che questi programmi possono eseguiti SOLO su sistemi Linux x86/x64
+;
+; Questi file sono i file grezzi.
+; Contengono diversi commenti per la spiegazione di ogni parte del codice.
+;
+; A breve svilupperò anche la versione .html con una guida più semplice da utilizzare e meglio strutturata.
+;
+; Per iniziare, seleziona il primo file (0001 sezioni.asm) qui a fianco.
